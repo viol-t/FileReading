f40a309dbcf9f5f0b9204c2ce0ef8bb05f08eea3
violet akpeh <violetakpeh@yahoo.com>
2022-12-08 13:35:24 +0100

    implemented method to read and save file

---
+++ src/main/java/org/example/ReadAndWriteCoursesList.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import javax.swing.*;
+import java.io.*;
+
+public class ReadAndWriteCoursesList {
+
+    public void readFile(String path){
+        //path = "src/main/resources/listofcourses.csv";
+        String line = "";
+        try (
+            BufferedReader br = new BufferedReader(new FileReader(path));
+        )
+        {
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void saveFile(String courseName, String filepath){
+
+        try (
+                BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(filepath, true));
+                PrintWriter printWriter = new PrintWriter(bufferedWriter);
+                ){
+
+            printWriter.println(courseName);
+            System.out.println("Record saved");
+
+        }
+        catch (IOException exception)
+        {
+            System.out.println("Record not saved");
+        }
+    }
+}
